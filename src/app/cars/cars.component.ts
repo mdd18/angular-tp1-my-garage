52b4f8a16622d87e2008e0967867eb50bde31eda
mathieu.douguet <mathieu.douguet@gmail.com>
2021-01-05 17:55:51 +0100

    commit en plus

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from './shared/car.model';
 
 @Component({
   selector: 'app-cars',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsComponent implements OnInit {
 
-  constructor() { }
+  cars: Car [];
+
+  constructor() {
+
+    let car1 = new Car(1,'Toyota','Yarris',100,1258,7800,5);
+    let car2 = new Car(2,'Peugeot','605',100,1258,7800,5);
+    
+    this.cars = [];
+    this.cars.push(car1);
+    this.cars.push(car2);
+
+  }
 
   ngOnInit(): void {
+  }
+
+
+  onSelect(car): void {
+
+
+
   }
 
 }
--- src/app/cars/shared/car.model.ts
+++ src/app/cars/shared/car.model.ts
@@ -12,6 +12,7 @@ export class Car {
         public nbSeats: number) {
 
             this._isRunning = false
+        
 
         }
 
