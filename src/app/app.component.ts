d9be46c82eeb8cf96317008b2a6a60127b6e81f3
mathieu.douguet <mathieu.douguet@gmail.com>
2021-01-05 12:46:29 +0100

    Premier commit test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-garage';
+  isEnable: boolean;
+  names: string [];
+
+
+
+  constructor() {
+    
+  }
+
 }
