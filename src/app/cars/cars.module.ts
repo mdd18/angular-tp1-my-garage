9b7d6e1b09222860077ddce277490665564072c3
mathieu.douguet <mathieu.douguet@gmail.com>
2021-01-06 14:47:18 +0100

    TP-Biding

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarsComponent } from './cars.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [CarsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [CarsComponent]
 })
